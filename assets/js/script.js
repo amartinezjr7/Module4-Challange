749db6a0adabb7236a9ea5ae4255eed94fc1d256
amartinezjr7 <94876426+amartinezjr7@users.noreply.github.com>
2022-01-01 19:37:08 -0800

    still debugging the quiz
    im going to have to redo the entire results section because i can't seem to pass in the variable I want

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 
 var timerEl = document.querySelector('#timer');
 var timeDis = document.querySelector('#str-btn');
+const quizContainer = document.getElementById('quiz')
+const resultContainer = document.getElementById('results')
 
 function timer(totalTime){
-    var timeLeft = 5;
+    var timeLeft = 60;
     console.log("it gets to here");
     var timeInterval = setInterval(function(){
         if(timeLeft > 1){
@@ -21,13 +23,74 @@ function timer(totalTime){
 
 }
 
+function quizBuilder(){
+    const output=[];
+
+    quizQuestions.forEach( (currentQuestion, questionNumber)=>{
+        const answers = [];
+
+        for(letter in currentQuestion.answers){
+            answers.push(
+                `<label>
+                <input type = "radio" onclick = "finalResults('b')" name="questions${questionNumber}" value="${letter}" id = "submit">
+                ${currentQuestion.answers[letter]}
+               
+                </label>`
+            )
+        }
+        output.push(
+            `<div class = "questions">${currentQuestion.question}</div>
+            <div class = "answers">${answers.join('')}</div>`
+        )
+        
+    })
+    quizContainer.innerHTML = output.join('');
+}
 
 
+const quizQuestions = [
+    {
+    question:"asdjhfasdjhfaslNEW ONE",
+    answers:{
+        a:"asdfasdf",
+        b:"sdfasdfa",
+        c:"asdfasdf",
+        d:"sdfasdfas",
+
+    },
+    correctAnswer: "a"
+    }
+];
+
+function finalResults(){
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    let numCorrect = 0;
+
+    quizQuestions.forEach((currentQuestion, questionNumber)=>{
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name = question${questionNumber}] : checked`;
+        const userAnswer = (answerContainer.querySelector(selector)|| {}).value;
+
+        if(userAnswer === currentQuestion.correctAnswer){
+            numCorrect++;
+            console.log("this is correct");
+            answerContainers[questionNumber].style.color = 'green';
+        }
+        else{
+            console.log("this is incorrect");
+            answerContainers[questionNumber].style.color = 'red'
+        }
+    })
+
+    resultContainer.innerHTML = `${numCorrect} out of ${quizQuestions.length}`;
+}
 
 timeDis.addEventListener('click',function(){
     timer();
     $('.title').remove();
     $('.hero').remove();
+    quizBuilder();
 });
 
 
